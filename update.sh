9b179983dbbd7167ad110cbf874aff5c6b7495d7
Kenji Saito <ken-yo@mbr.nifty.com>
2026-09-07 23:40:37 +0900

    Bumps node modules

--- update.sh
+++ update.sh
@@ -15,7 +15,7 @@ fi
 echo ""
 pwd
 
-if ! (disable-checkout-persist-credentials && rm -rf pnpm-lock.yaml node_modules && pnx pnpm@latest self-update && pnpm install --no-frozen-lockfile && pnpm clean --lockfile && pnpm up -r && pnpm audit --fix override && pnpm up -r && pnpm -r --if-present lint-fix && pnpm -r --if-present build && pnpm -r --if-present test && pnpm install --no-frozen-lockfile); then
+if ! (disable-checkout-persist-credentials && rm -rf pnpm-lock.yaml node_modules && pnx pnpm@latest self-update && pnpm install --no-frozen-lockfile && pnpm clean --lockfile && pnpm up -r --include-github-actions && pnpm audit --fix override && pnpm up -r && pnpm -r --if-present lint-fix && pnpm -r --if-present build && pnpm -r --if-present test && pnpm install --no-frozen-lockfile); then
   cd "${CUR}" || exit
   exit 1
 fi
